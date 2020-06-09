63c4fc89bd62b39d2b086ce7c1ec03ce9c083347
urilivshits <65599467+urilivshits@users.noreply.github.com>
2020-06-09 13:16:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -142,10 +142,10 @@ $('.minutes').on('scroll', function() {
             $(this).trigger("mouseleave");
             function choose () {
                 minutes = ($(this).text());
-            //     setTimeout(delayF, 50);
-            //     function delayF () {
+                setTimeout(delayF, 150);
+                function delayF () {
                 $(".minutes p")[minutes].scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
-            // }
+            }
             }
         } 
         else {
