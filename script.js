e2c014a128fc7df10ae1d823fb8ac8bd5621f1f3
urilivshits <65599467+urilivshits@users.noreply.github.com>
2020-06-09 12:45:22 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -99,10 +99,10 @@ $('.seconds').on('scroll', function() {
             $(this).trigger("click");
             function choose () {
                 seconds = $(this).text();
-                    setTimeout(delayF, 1000);
-                    function delayF () {
-                    $(".seconds p")[seconds].scrollIntoView({behavior: "auto", block: "center", inline: "nearest"});
-            }
+                $(".seconds p")[seconds].scrollIntoView({behavior: "auto", block: "center", inline: "nearest"});
+            //         setTimeout(delayF, 1000);
+            //         function delayF () {
+            // }
             }
         } 
         else {
