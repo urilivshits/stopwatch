657ceea80ae6be0185cef408008dac9472b5cfc6
urilivshits <65599467+urilivshits@users.noreply.github.com>
2020-06-13 12:02:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -169,7 +169,7 @@ $('.seconds').on('scroll', function() {
             }
             timerSeconds = setTimeout(function () {
             $(".seconds p")[seconds].scrollIntoView({behavior: "smooth", block: "center"});
-            }, 10);
+            }, 50);
         } 
         else {
             $(this).css("font-weight", "normal");
